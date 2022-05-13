358ba124bc1ad17316eed5c65c9bdaa47a2fdede
EAS-Clark <clark@enterpriseautomation.co.uk>
2022-05-13 10:16:42 +0100

    more testing

--- features/test.feature
+++ features/test.feature
@@ -7,13 +7,14 @@ Scenario Outline: get categories
 
 Scenario Outline: get categories by id 
     When I send a GET request to http://localhost:3000/api/categories/1
-    Then I receive a response name of sdfsdfsdf
-
-Scenario Outline: patch categories by id 
-    When I send a PATCH request to http://localhost:3000/api/categories/1  
-    And I send the data of {"name": "test"}
-    Then I receive a response name of "rows edited: 1"
-
-Scenario Outline: get categories by id 
-    When I send a GET request to http://localhost:3000/api/categories/1
     Then I receive a response name of test
+
+Scenario Outline: modify contact
+    When I send PATCH request with a {"name": "test"} to http://localhost:3000/api/categories/1
+    Then I get response code 200
+
+Scenario Outline: uploading contact
+    When I send POST request with a {"name": "test"} to http://localhost:3000/api/categories/1
+    Then I get response code 200
+
+
--- steps/test.js
+++ steps/test.js
@@ -3,13 +3,25 @@ const { assert } = require('chai');
 const { response } = require('express');
 const restHelper = require('../src/utils/restHelper.js');
 
-When('I send a GET request to {}', async function (path) {
-    const response = await restHelper.getData(`${path}`);
+When('I send a GET request to {}', async function (url) {
+    const response = await restHelper.getData(`${url}`);
     this.response = response;
 });
 
-When('I send a PATCH request to {}', async function (path) {
-    const response = await restHelper.getData(`${path}`);
+When('I send a DELETE request to {}', async function (url) {
+    const response = await restHelper.deleteData(`${url}`);
+    this.response = response;
+});
+
+When('I send PATCH request with a {} to {}', async function (data, url) {
+    const response = await restHelper.patchData(`${url}`, JSON.parse(data));
+    console.log('patch test: ' + JSON.stringify(response.data));
+    this.response = response;
+});
+
+When('I send POST request with a {} to {}', async function (data, url) {
+    const response = await restHelper.postData(`${url}`, JSON.parse(data));
+    console.log('patch test: ' + JSON.stringify(response.data));
     this.response = response;
 });
 
@@ -18,6 +30,14 @@ Then('I receive a status code of {}', async function (status) {
 });
 
 
-Then('I receive a response name of {}', async function (status) {
-    assert.equal(this.response.data.response.name, status);
+Then('I receive a response name of {}', async function (name) {
+    assert.equal(this.response.data.response.name, name);
 });
+
+Then('I receive a response of {}', async function (response) {
+    assert.equal(this.response.data.response, response);
+});
+
+
+
+
